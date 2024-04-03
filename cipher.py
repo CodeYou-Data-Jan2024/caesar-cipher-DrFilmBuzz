719cc547998738bfa3dc01f0295218c48ed02d0d
DrFilmBuzz <wesley_gift@yahoo.com>
2024-04-03 19:27:58 -0400

    Class example

--- cipher.py
+++ cipher.py
@@ -1,2 +1,23 @@
 # Wesley Gift Caesar Cipher Homework Submission
 # Implement a Caesar Cipher with a right shift of 5
+# Ask user to enter a phrase
+print("Input phrase:")
+phrase = input()
+# Show entered phrase to user
+print("You entered: " + phrase)
+# Provide alphabet for code
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+new_phrase = ""
+
+for i in range(0, len(phrase)):
+    try: 
+        char = phrase[i]
+        new_char = str(alphabet[alphabet.index(char)+5])
+
+        new_phrase += new_char
+
+    except: 
+        new_char = str(alphabet[0])
+        new_phrase += new_char
+# Prompt code to print new phrase
+print("The Cipher Phrase is" + new_phrase)
